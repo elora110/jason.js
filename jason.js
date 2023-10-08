2a3770ecb666b609c8f616f21e1c144bceb96ce0
elora110 <146921734+elora110@users.noreply.github.com>
2023-10-08 20:38:44 +0200

    Add files via upload

---
+++ jason.js
@@ -0,0 +1,18 @@
+let nom = "jason"
+let pv = 100 
+let tableaunom = ["clara blonde", "lea sportive", "alexandre nerd", "nicolas roux", "mathilde brune"];
+//tableau nom des survivants 
+
+let nomchosi;
+for (let i=100; i<0;i++){
+    // Générer un nombre aléatoire entre 0 et 4 pour choisir un nom
+nomchosi = Math.floor(Math.random() * 5);
+if (santemental<=0){
+    console.log("john a epuisé sa santé mental");
+    i=0;
+};
+};
+//i=0 pour arreter la boucle quand pv jason = 0
+//boucle for pour choisir un nom (attaque ou mort) aléatoirement 
+//je ne comprend pas comment faire les actions
+//ni l'enoncer 
